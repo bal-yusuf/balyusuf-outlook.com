bf0eb3fab078ce4bf34b7ab0b427ed6ff78fb866
bal-yusuf <balyusuf@outlook.com>
2020-08-12 01:07:54 +0300

    second commit

--- home/models.py
+++ home/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Settings(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+
+    title = models.CharField(max_length=150)
+    keywords = models.CharField(blank=True, max_length=255)
+    description = models.CharField(max_length=255)
+    icon = models.ImageField(blank=True, upload_to='images/')
+    company = models.CharField(blank=True, max_length=255)
+    address = models.CharField(blank=True, max_length=255)
+    fax = models.CharField(blank=True, max_length=255)
+    email = models.CharField(blank=True, max_length=255)
+    smtpserver = models.CharField(blank=True, max_length=255)
+    smtpemail = models.CharField(blank=True, max_length=255)
+    smtppassword = models.CharField(blank=True, max_length=255)
+    smtpport = models.CharField(blank=True, max_length=255)
+    facebook = models.CharField(blank=True, max_length=255)
+    instagram = models.CharField(blank=True, max_length=255)
+    twitter = models.CharField(blank=True, max_length=255)
+    phone = models.CharField(blank=True, max_length=255)
+    about_us = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- projectSum/settings.py
+++ projectSum/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
+    'product.apps.ProductConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URl = '/uploads/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
